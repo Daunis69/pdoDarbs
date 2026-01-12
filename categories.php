245b363996a9dc3e4268d0010c2be765f44aa27f
Daunis69 <berzinspavils@gmail.com>
2026-01-12 21:39:28 +0200

    Kategorijas izveidots kopa ar categorie.php

---
+++ categories.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "functions.php";
+require_once "Database.php";
+$config = require "config.php";
+
+$db = new Database($config["database"]);
+$sql_query = "SELECT * FROM categories";
+$params = [];
+
+if(isset($_GET["search_query"]) && trim($_GET["search_query"]) != "") {
+    $sql_query = "SELECT * FROM categories WHERE category_name LIKE :search";
+    $params["search"] = "%" . $_GET["search_query"] . "%";
+}
+
+$categories = $db->query($sql_query, $params)->fetchAll(PDO::FETCH_ASSOC);
+echo "<h1>Kategorijas</h1>";
+echo "<form method='get'>";
+echo "<input name='search_query' />";
+echo "<button>Meklēt</button>";
+echo "</form>";
+echo "<ul>";
+
+foreach($categories as $category) {
+    echo "<li>" . $category["category_name"] . "</li>";
+}
+echo "</ul>";
--- migration.sql
+++ migration.sql
@@ -10,3 +10,13 @@ INSERT INTO posts
 VALUES
 ("Lieldienas nāk"),
 ("Otrais bloga ieraksts");
+
+CREATE TABLE categories(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+category_name VARCHAR(5200) NOT NULL
+INSERT INTO categories
+(category_name)
+VALUES
+("Svētki"),
+("Mūzika"),
+("Sports");
