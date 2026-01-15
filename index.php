11ebdeb8c00eb4ff335f56bd2087b6babab30cfa
Daunis69 <berzinspavils@gmail.com>
2026-01-15 12:41:44 +0200

    4 nodalas sakums

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once "functions.php";
 require_once "Database.php";
 $config = require "config.php";
 
+
 $db = new Database($config["database"]);
 $sql_query = "SELECT * FROM posts";
 $params = [];
@@ -14,14 +15,8 @@ if(isset($_GET["search_query"]) && trim($_GET["search_query"]) != "") {
 }
 
 $posts = $db->query($sql_query, $params)->fetchAll(PDO::FETCH_ASSOC);
-echo "<h1> Blogs </h1>";
-echo "<form method='get'>";
-echo "<input name='search_query' />";
-echo "<button>Meklēt</button>";
-echo "</form>";
-echo "<ul>";
 
-foreach($posts as $post) {
-    echo "<li>" . $post["content"] . "</li>";
-}
-echo "</ul>";
+
+
+
+require "./views/index.view.php";
