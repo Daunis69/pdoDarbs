0d713d9778ac9644b835836eacc89e5ee6453459
Daunis69 <berzinspavils@gmail.com>
2026-02-11 11:23:20 +0200

    Update config

--- config.php
+++ config.php
@@ -3,9 +3,9 @@ return [
     "database"=>[
             "host" => "localhost",
             "port" => 3306,
-            "user" => "root",
-            "password" => "",
-            "dbname" => "blog",
+            "user" => "projekts_lieladesa",
+            "password" => "lieladesa12",
+            "dbname" => "projekts_lieladesa",
             "charset" => "utf8mb4"
 ],
          "email" => [],
